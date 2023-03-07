1e07f0c4483bf935f2c8fc76138b9988c7ff7937
Helen Griffiths <16269977+threepistons@users.noreply.github.com>
2023-03-07 23:02:35 +0000

    Add unit tests to 100% code coverage

--- spec/classes/lithnetamagent_spec.rb
+++ spec/classes/lithnetamagent_spec.rb
@@ -3,25 +3,62 @@
 require 'spec_helper'
 
 describe 'lithnetamagent' do
-  context 'Supported OS tests' do
-    weuse = {
-      supported_os: [
-        {
-          'operatingsystem'        => 'Ubuntu',
-          'operatingsystemrelease' => ['18.04', '20.04', '22.04'],
-        },
-        {
-          'operatingsystem'        => 'RedHat',
-          'operatingsystemrelease' => ['7', '8', '9'],
-        },
-      ],
-      hardwaremodels: ['amd64', 'x86_64'],
-    }
-    on_supported_os(weuse).each do |_os, os_facts|
+  ubuntu = {
+    supported_os: [
+      {
+        'operatingsystem'        => 'Ubuntu',
+        'operatingsystemrelease' => ['20.04', '22.04'],
+      },
+    ],
+  }
+  redhat = {
+    supported_os: [
+      {
+        'operatingsystem'        => 'RedHat',
+        'operatingsystemrelease' => ['7', '8', '9'],
+      },
+      {
+        'operatingsystem'        => 'CentOS',
+        'operatingsystemrelease' => ['7'],
+      },
+    ],
+  }
+  settings = {
+    'register_agent' => true,
+    'ams_server'     => '192.168.1.100',
+    'reg_key'        => 'alphabetsoup'
+  }
+  context 'Supported OS tests with parameters' do
+    let(:params) { settings }
+
+    on_supported_os.each do |_os, os_facts|
       let(:facts) { os_facts }
 
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_package('LithnetAccessManagerAgent') }
-    end
-  end
-end
+      it {
+        is_expected.to contain_exec('agent-register').with(
+          'command' => '/opt/LithnetAccessManagerAgent/Lithnet.AccessManager.Agent --server 192.168.1.100 --registration-key alphabetsoup',
+        )
+      }
+    end # on_supported_os each
+  end # with-params context
+  context 'Ubuntu-only tests' do
+    let(:params) { settings }
+
+    on_supported_os(ubuntu).each do |_os, os_facts|
+      let(:facts) { os_facts }
+
+      it { is_expected.to contain_apt__source('Lithnet') }
+    end # ubuntu each
+  end # Ubuntu context
+  context 'RedHat-only tests' do
+    let(:params) { settings }
+
+    on_supported_os(redhat).each do |_os, os_facts|
+      let(:facts) { os_facts }
+
+      it { is_expected.to contain_yumrepo('lithnet-am-repo') }
+    end # redhat each
+  end # RedHat context
+end # describe
